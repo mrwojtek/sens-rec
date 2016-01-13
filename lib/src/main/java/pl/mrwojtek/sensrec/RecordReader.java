e3d766be6c6729c9792cf7fc705a8dbace498682
Wojciech Mruczkiewicz <mrwojtek@mrwojtek.pl>
2016-01-13 19:16:05 +0100

    Clean up RecordReader class

--- lib/src/main/java/pl/mrwojtek/sensrec/RecordReader.java
+++ lib/src/main/java/pl/mrwojtek/sensrec/RecordReader.java
@@ -43,23 +43,29 @@ public class RecordReader {
     protected static final int END_BINARY_LENGTH_1200 = 52 + SensorsRecorder.MAGIC_WORD.length();
     protected static final int END_TEXT_EXPECTED_LENGTH = 70 + SensorsRecorder.MAGIC_WORD.length();
     protected static final int END_TEXT_MAX_LENGTH = 1024;
+    protected static final int BUFFER_SIZE = 1024;
 
     protected SensorsRecorder recorder;
-    protected Date startDate;
-    protected Date endDate;
-    protected Long duration;
-    protected Long movingTime;
-    protected Double length;
-    protected Long endMilliseconds;
-    protected long startMilliseconds;
-    protected int version;
+
+    // Start frame data
     protected boolean binary;
+    protected int version;
+    protected Date startDate;
+    protected long startTime;
+
+    // End frame data
+    protected Date endDate;
+    protected Long endTime;
+    protected Long duration;
+    protected Long movingDuration;
+    protected Double totalDistance;
 
     protected byte[] startPrefix;
     protected byte[] endPrefix;
     protected byte[] magicBytes;
     protected byte[] magicWord;
     protected byte[] newLineBytes;
+    protected byte[] buffer;
 
     public RecordReader(SensorsRecorder recorder) {
         this.recorder = recorder;
@@ -72,22 +78,23 @@ public class RecordReader {
         magicBytes = SensorsRecorder.MAGIC_WORD.getBytes();
         magicWord = new byte[magicBytes.length];
         newLineBytes = recorder.getTextNewLine().getBytes();
+        buffer = new byte[BUFFER_SIZE];
     }
 
     public Date getStartDate() {
         return startDate;
     }
 
-    public long getStartMilliseconds() {
-        return startMilliseconds;
+    public long getStartTime() {
+        return startTime;
     }
 
     public Long getDuration() {
         return duration;
     }
 
-    public Double getLength() {
-        return length;
+    public Double getTotalDistance() {
+        return totalDistance;
     }
 
     public int getVersion() {
@@ -101,7 +108,7 @@ public class RecordReader {
     public boolean readStart(File file) {
         RandomAccessFile raf = null;
         try {
-            return readStart(raf = new RandomAccessFile(file, "r"), new byte[1024]);
+            return readStart(raf = new RandomAccessFile(file, "r"));
         } catch (FileNotFoundException ex) {
             Log.e(TAG, "Record file not found: " + ex.getMessage());
         } catch (IOException ex) {
@@ -118,18 +125,12 @@ public class RecordReader {
         return false;
     }
 
-    private boolean readStart(RandomAccessFile raf, byte[] buffer) throws IOException {
-        int read = raf.read(buffer, 0, Math.max(START_BINARY_LENGTH, startPrefix.length));
-        return tryStartBinary(buffer, read) || tryStartText(buffer, read, raf);
-    }
-
     public boolean readStartEnd(File file) {
         RandomAccessFile raf = null;
         try {
             raf = new RandomAccessFile(file, "r");
-            byte[] buffer = new byte[1024];
-            if (readStart(raf, buffer)) {
-                if (!readEnd(raf, buffer)) {
+            if (readStart(raf)) {
+                if (!readEnd(raf)) {
                     clearEndData();
                 }
                 return true;
@@ -151,187 +152,221 @@ public class RecordReader {
     }
 
     private void clearEndData() {
-        endMilliseconds = null;
+        endTime = null;
         endDate = null;
         duration = null;
-        length = null;
+        movingDuration = null;
+        totalDistance = null;
     }
 
-    private boolean tryStartBinary(byte[] buffer, int read) throws IOException {
+    private boolean readStart(RandomAccessFile raf) throws IOException {
+        int read = raf.read(buffer, 0, Math.max(START_BINARY_LENGTH, startPrefix.length));
+        return tryStartBinary(read) || tryStartText(read, raf);
+    }
+
+    private boolean tryStartBinary(int read) throws IOException {
         if (read >= START_BINARY_LENGTH) {
             DataInputStream dis = new DataInputStream(new ByteArrayInputStream(buffer));
-            if (dis.readShort() == SensorsRecorder.TYPE_START &&
-                    dis.readShort() == 0 &&
-                    dis.readInt() == magicBytes.length) {
-                if (dis.read(magicWord, 0, magicWord.length) == magicWord.length &&
-                        Arrays.equals(magicBytes, magicWord)) {
-                    version = dis.readInt();
-                    if (version == 1100 || version == 1200) {
-                        startMilliseconds = dis.readLong();
-                        startDate = new Date(dis.readLong());
-                        binary = true;
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
 
-    private boolean tryStartText(byte[] buffer, int read, RandomAccessFile raf) throws IOException {
-        if (read >= startPrefix.length) {
-            if (!equals(buffer, 0, startPrefix, 0, startPrefix.length)) {
+            // Check start frame header
+            if (dis.readShort() != SensorsRecorder.TYPE_START ||
+                    dis.readShort() != 0 ||
+                    dis.readInt() != magicBytes.length) {
                 return false;
             }
 
-            raf.seek(startPrefix.length);
-            Scanner scanner = new Scanner(raf.getChannel());
-            if (scanner.hasNextInt()) {
-                version = scanner.nextInt();
-                if (version == 1100 || version == 1200) {
-                    if (!scanner.hasNextLong()) {
-                        return false;
-                    } else {
-                        startMilliseconds = scanner.nextLong();
-                    }
-
-                    if (!scanner.hasNextLong()) {
-                        return false;
-                    } else {
-                        startDate = new Date(scanner.nextLong());
-                    }
-
-                    return true;
-                }
+            // Check magic word
+            if (dis.read(magicWord, 0, magicWord.length) != magicWord.length ||
+                    !Arrays.equals(magicBytes, magicWord)) {
+                return false;
             }
-        }
-        return false;
-    }
 
-    private boolean readEnd(RandomAccessFile raf, byte[] buffer) throws IOException {
-        if (binary) {
-            return tryEndBinary(buffer, raf);
-        } else {
-            return tryEndText(buffer, raf);
-        }
-    }
-
-    private boolean tryEndBinary(byte[] buffer, RandomAccessFile raf) throws IOException {
-        int binaryLength = version == 1200 ? END_BINARY_LENGTH_1200 : END_BINARY_LENGTH_1100;
-        if (raf.length() > binaryLength) {
-            raf.seek(raf.length() - binaryLength);
-            if (raf.read(buffer, 0, binaryLength) == binaryLength) {
-                DataInputStream dis = new DataInputStream(new ByteArrayInputStream(buffer));
-
-                // Check header
-                if (dis.readShort() != SensorsRecorder.TYPE_END ||
-                        dis.readShort() != 0 ||
-                        dis.readInt() != magicBytes.length) {
-                    return false;
-                }
-
-                // Check magic word
-                if (dis.read(magicWord, 0, magicWord.length) != magicWord.length ||
-                        !Arrays.equals(magicBytes, magicWord) ||
-                        dis.readInt() != version) {
-                    return false;
-                }
-
-                // Read data
-                endMilliseconds = dis.readLong();
-                endDate = new Date(dis.readLong());
-                if (version == 1200) {
-                    duration = dis.readLong();
-                    movingTime = dis.readLong();
-                } else {
-                    duration = endMilliseconds - startMilliseconds;
-                }
-                length = dis.readDouble();
+            // Resolve start frame version and read data
+            version = dis.readInt();
+            if (version == 1100 || version == 1200) {
+                startTime = dis.readLong();
+                startDate = new Date(dis.readLong());
+                binary = true;
                 return true;
             }
         }
         return false;
     }
 
-    private boolean tryEndText(byte[] buffer, RandomAccessFile raf) throws IOException {
+    private boolean tryStartText(int read, RandomAccessFile raf) throws IOException {
+        if (read >= startPrefix.length) {
+            // Check start frame header and magic word
+            if (!equals(buffer, 0, startPrefix, 0, startPrefix.length)) {
+                return false;
+            }
+
+            raf.seek(startPrefix.length);
+            Scanner scanner = new Scanner(raf.getChannel());
+            if (!scanner.hasNextInt()) {
+                return false;
+            }
+
+            // Resolve start frame version and read data
+            version = scanner.nextInt();
+            if (version == 1100 || version == 1200) {
+                if (!scanner.hasNextLong()) {
+                    return false;
+                }
+                startTime = scanner.nextLong();
+
+                if (!scanner.hasNextLong()) {
+                    return false;
+                }
+                startDate = new Date(scanner.nextLong());
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean readEnd(RandomAccessFile raf) throws IOException {
+        if (binary) {
+            return tryEndBinary(raf);
+        } else {
+            return tryEndText(raf);
+        }
+    }
+
+    private boolean tryEndBinary(RandomAccessFile raf) throws IOException {
+        int binaryLength = version == 1200 ? END_BINARY_LENGTH_1200 : END_BINARY_LENGTH_1100;
+        if (raf.length() > binaryLength) {
+            // Read last bytes that could potentially be an ending frame from file
+            raf.seek(raf.length() - binaryLength);
+            if (raf.read(buffer, 0, binaryLength) != binaryLength) {
+                return false;
+            }
+
+            DataInputStream dis = new DataInputStream(new ByteArrayInputStream(buffer));
+
+            // Check end frame header
+            if (dis.readShort() != SensorsRecorder.TYPE_END ||
+                    dis.readShort() != 0 ||
+                    dis.readInt() != magicBytes.length) {
+                return false;
+            }
+
+            // Check magic word
+            if (dis.read(magicWord, 0, magicWord.length) != magicWord.length ||
+                    !Arrays.equals(magicBytes, magicWord) ||
+                    dis.readInt() != version) {
+                return false;
+            }
+
+            // Read end frame data
+            if (version == 1100 || version == 1200) {
+                endTime = dis.readLong();
+                endDate = new Date(dis.readLong());
+
+                if (version == 1200) {
+                    duration = dis.readLong();
+                    movingDuration = dis.readLong();
+                } else {
+                    duration = endTime - startTime;
+                }
+
+                totalDistance = dis.readDouble();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean tryEndText(RandomAccessFile raf) throws IOException {
+        // First, try to parse end frame of expected length
         long seek = Math.max(0, raf.length() - END_TEXT_EXPECTED_LENGTH);
         int count = (int) (raf.length() - seek);
         int pos = buffer.length - count;
+
+        // Read expected length bytes
         raf.seek(seek);
         if (raf.read(buffer, pos, count) != count) {
             return false;
         }
 
-        int res = tryEndText(buffer, pos);
+        int res = tryEndText(pos);
         if (res == 0) {
             return true;
         } else if (res == 1) {
             return false;
         } else {
+            // Frame was not resolved, try the maximum size
             long secondSeek = Math.max(0, raf.length() - END_TEXT_MAX_LENGTH);
             if (secondSeek == seek) {
                 return false;
             }
             count = (int) (seek - secondSeek);
             pos -= count;
+
+            // Read the missing bytes
             raf.seek(secondSeek);
-            return raf.read(buffer, pos, count) == count &&
-                    tryEndText(buffer, pos) == 0;
+            return raf.read(buffer, pos, count) == count && tryEndText(pos) == 0;
         }
     }
 
-    private int tryEndText(byte[] buffer, int pos) {
+    private int tryEndText(int pos) {
         int end = buffer.length;
-        if (end - newLineBytes.length > pos &&
-                equals(buffer, end - newLineBytes.length, newLineBytes, 0, newLineBytes.length)) {
-            end -= newLineBytes.length;
+        int endNewLine = end - newLineBytes.length;
+        if (endNewLine > pos && equals(buffer, endNewLine, newLineBytes, 0, newLineBytes.length)) {
+            end = endNewLine;
         }
 
+        // Search for end frame preceeding new line
         for (int p = end - 1; p >= pos; --p) {
             if (equals(buffer, p, newLineBytes, 0, newLineBytes.length)) {
+                // New line found, skip it
                 p += newLineBytes.length;
+
+                // Match end frame header
                 if (!equals(buffer, p, endPrefix, 0, endPrefix.length)) {
                     return 1;
                 }
-
                 p += endPrefix.length;
 
+                // Resolve version
                 Scanner scanner = new Scanner(new ByteArrayInputStream(buffer, p, end - p));
-                if (!scanner.hasNextInt() ||
-                        scanner.nextInt() != version) {
+                if (!scanner.hasNextInt() || scanner.nextInt() != version) {
                     return 1;
                 }
 
-                if (!scanner.hasNextLong()) {
-                    return 1;
-                }
-                endMilliseconds = scanner.nextLong();
-
-                if (!scanner.hasNextLong()) {
-                    return 1;
-                }
-                endDate = new Date(scanner.nextLong());
-
-                if (version == 1200) {
+                // Read end frame data
+                if (version == 1100 || version == 1200) {
                     if (!scanner.hasNextLong()) {
                         return 1;
                     }
-                    duration = scanner.nextLong();
+                    endTime = scanner.nextLong();
 
                     if (!scanner.hasNextLong()) {
                         return 1;
                     }
-                    movingTime = scanner.nextLong();
-                } else {
-                    duration = endMilliseconds - startMilliseconds;
-                }
+                    endDate = new Date(scanner.nextLong());
 
-                if (!scanner.hasNextDouble()) {
-                    return 1;
-                }
-                length = scanner.nextDouble();
+                    if (version == 1200) {
+                        if (!scanner.hasNextLong()) {
+                            return 1;
+                        }
+                        duration = scanner.nextLong();
 
-                return 0;
+                        if (!scanner.hasNextLong()) {
+                            return 1;
+                        }
+                        movingDuration = scanner.nextLong();
+                    } else {
+                        duration = endTime - startTime;
+                    }
+
+                    if (!scanner.hasNextDouble()) {
+                        return 1;
+                    }
+                    totalDistance = scanner.nextDouble();
+
+                    return 0;
+                }
             }
         }
 
