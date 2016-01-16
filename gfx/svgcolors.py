cf4f9fb89ec092116c2e3cb89d13112d4450ff43
Wojciech Mruczkiewicz <mrwojtek@mrwojtek.pl>
2016-01-16 11:48:31 +0100

    Fix copyright notice

--- gfx/svgcolors.py
+++ gfx/svgcolors.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-   (C) Copyright 2015, 2106 Wojciech Mruczkiewicz
+   (C) Copyright 2015, 2016 Wojciech Mruczkiewicz
    
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
